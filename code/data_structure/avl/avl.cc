f9c981a133ef911ca2235ead650253da3a132712
WangJiaLe <15768089+wangs-joyful-home@user.noreply.gitee.com>
2026-02-05 10:51:32 +0800

    完善avl代码

--- code/data_structure/avl/avl.cc
+++ code/data_structure/avl/avl.cc
@@ -1,47 +1,46 @@
 #include <iostream>
-#include <assert.h>
+#include <cassert>
 using namespace std;
 
-template <class T, class V>
+template<class K, class V>
 class AVLNode
 {
 public:  
-    AVLNode(const T& key = T(), const V& val = T())
-        : key(key)
-        , val(val)
+    AVLNode(pair<K, V>& kv)
+        : kv(kv)
+        , bf(0)
+        , parent(nullptr)
+        , right(nullptr)
+        , left(nullptr)
     {}
-
-    T key;
-    V val;
-    int bf = 0; //平衡因子
-    AVLNode* parent = nullptr;
-    AVLNode* left = nullptr;
-    AVLNode* right = nullptr;
+    pair<K, V> kv;
+    int bf;
+    AVLNode* parent;
+    AVLNode* right;
+    AVLNode* left;
 };
 
-template <class T, class V>
+template <class K, class V>
 class AVLTree
 {
-public:  
-    typedef AVLNode<T, V> Node;
-    bool insert(const T& key = T(), const V& val = V())
+public:    
+    typedef AVLNode<K, V> Node;
+    bool insert(pair<K, V> kv)
     {
         if(_root == nullptr)
         {
-            _root = new Node(key, val);
+            _root = new Node(kv);
             return true;
         }
-
-        Node* parent = nullptr;
-        Node* cur = _root; 
+        Node* parent = nullptr, *cur = _root;
         while(cur)
         {
-            if(cur->key < key)
+            if(cur->kv.first < kv.first)
             {
                 parent = cur;
                 cur = cur->right;
             }
-            else if(cur->key > key)
+            else if(cur->kv.first > kv.first)
             {
                 parent = cur;
                 cur = cur->left;
@@ -52,14 +51,14 @@ public:
             }
         }
 
-        cur = new Node(key, val);
-        if(key > parent->key)
+        cur = new Node(kv);
+        if(cur->kv.first < parent->kv.first)
         {
-            parent->right = cur;
+            parent->left = cur;
         }
         else
         {
-            parent->left = cur;
+            parent->right = cur;
         }
         cur->parent = parent;
 
@@ -67,11 +66,11 @@ public:
         {
             if(parent->left == cur)
             {
-                (parent->bf)--;
+                parent->bf--;
             }
             else
             {
-                (parent->bf)++;
+                parent->bf++;
             }
             if(parent->bf == 0)
             {
@@ -85,21 +84,21 @@ public:
             else if(parent->bf == 2 || parent->bf == -2)
             {
                 //旋转处理
-                if(parent->bf == 2 && cur->bf == 1)
-                {
-                    RotateL(parent);
-                }
-                else if(parent->bf == 2 && cur->bf == -1)
-                {
-                    RotateRL(parent);
-                }
-                else if(parent->bf == -2 && cur->bf == -1)
+                if(parent->bf == -2 && cur->bf == -1)
                 {
                     RotateR(parent);
                 }
                 else if(parent->bf == -2 && cur->bf == 1)
                 {
                     RotateLR(parent);
+                }
+                else if(parent->bf == 2 && cur->bf == 1)
+                {
+                    RotateL(parent);
+                }
+                else if(parent->bf == 2 && cur->bf == -1)
+                {
+                    RotateRL(parent);
                 }
                 else
                 {
@@ -117,14 +116,17 @@ public:
 
     void RotateR(Node* parent)
     {
-        if(parent == nullptr)
-        {
-            return;
-        }
         Node* subL = parent->left;
         Node* subLR = subL->right;
         Node* grandparent = parent->parent;
 
+        parent->parent = subL;
+        parent->left = subLR;
+        if(subLR)
+        {
+            subLR->parent = parent;
+        }
+        subL->right = parent;
         if(parent == _root)
         {
             _root = subL;
@@ -132,37 +134,34 @@ public:
         }
         else
         {
-            if(grandparent->right == parent)
-            {
-                grandparent->right = subL;
-            }
-            else
+            if(grandparent->left == parent)
             {
                 grandparent->left = subL;
             }
+            else
+            {
+                grandparent->right = subL;
+            }
             subL->parent = grandparent;
         }
-        parent->parent = subL;
-        parent->left = subLR;
-        subL->right = parent;
-        if(subLR)
-        {
-            subLR->parent = parent;
-        }
+
         subL->bf = 0;
         parent->bf = 0;
     }
 
     void RotateL(Node* parent)
     {
-        if(parent == nullptr)
-        {
-            return;
-        }
-        Node* grandparent = parent->parent;
         Node* subR = parent->right;
         Node* subRL = subR->left;
+        Node* grandparent = parent->parent;
 
+        subR->left = parent;
+        parent->parent = subR;
+        parent->right = subRL;
+        if(subRL)
+        {
+            subRL->parent = parent;
+        }
         if(parent == _root)
         {
             _root = subR;
@@ -170,34 +169,22 @@ public:
         }
         else
         {
-            if(grandparent->right == parent)
-            {
-                grandparent->right = subR;
-            }
-            else
+            if(grandparent->left == parent)
             {
                 grandparent->left = subR;
             }
+            else
+            {
+                grandparent->right = subR;
+            }
             subR->parent = grandparent;
         }
-        parent->right = subRL;
-        if(subRL)
-        {
-            subRL->parent = parent;
-        }
-        parent->parent = subR;
-        subR->left = parent;
-
         subR->bf = 0;
         parent->bf = 0;
     }
 
     void RotateLR(Node* parent)
     {
-        if(parent == nullptr)
-        {
-            return;
-        }
         Node* subL = parent->left;
         Node* subLR = subL->right;
         int bf = subLR->bf;
@@ -205,7 +192,13 @@ public:
         RotateL(subL);
         RotateR(parent);
 
-        if(bf == 1)
+        if(bf == 0)
+        {
+            subL->bf = 0;
+            subLR->bf = 0;
+            parent->bf = 0;
+        }
+        else if(bf == 1)
         {
             subL->bf = -1;
             subLR->bf = 0;
@@ -215,11 +208,7 @@ public:
         {
             subL->bf = 0;
             subLR->bf = 0;
-            parent->bf = 1;
-        }
-        else if(bf == 0)
-        {
-            subL->bf = subLR->bf = parent->bf = 0;
+            parent->bf = -1;
         }
         else
         {
@@ -229,10 +218,6 @@ public:
 
     void RotateRL(Node* parent)
     {
-        if(parent == nullptr)
-        {
-            return;
-        }
         Node* subR = parent->right;
         Node* subRL = subR->left;
         int bf = subRL->bf;
@@ -240,7 +225,13 @@ public:
         RotateR(subR);
         RotateL(parent);
 
-        if(bf == 1)
+        if(bf == 0)
+        {
+            subR->bf = 0;
+            subRL->bf = 0;
+            parent->bf = 0;
+        }
+        else if(bf == 1)
         {
             subR->bf = 0;
             subRL->bf = 0;
@@ -252,24 +243,20 @@ public:
             subRL->bf = 0;
             parent->bf = 0;
         }
-        else if(bf == 0)
-        {
-            subR->bf = subRL->bf = parent->bf = 0;
-        }
         else
         {
             assert(false);
         }
     }
 
-    Node* root()
-    {
-        return _root;
-    }
-
     void inOrder()
     {
         _inOrder(_root);
+    }
+
+    bool test()
+    {
+        return _test(_root);
     }
 private:
     void _inOrder(Node* root)
@@ -279,61 +266,59 @@ private:
             return;
         }
         _inOrder(root->left);
-        cout << root->key << ":" << root->val << "-" << root->bf << endl;
+        cout << root->kv.first << " " << root->kv.second << " " << root->bf << endl;
         _inOrder(root->right);
     }
 
+    int _height(Node* root)
+    {
+        if(root == nullptr)
+        {
+            return 0;
+        }
+        int lh = _height(root->left);
+        int rh = _height(root->right);
+        return lh > rh ? lh + 1 : rh + 1;
+    }
+
+    bool _test(Node* root)
+    {
+        if(root == nullptr)
+        {
+            return true;
+        }
+        int diff = _height(root->right) - _height(root->left);
+        if(abs(diff) > 1)
+        {
+            cout << "bad height: " << root->kv.first << endl;
+        }
+        if(diff != root->bf)
+        {
+            cout << "bad bf: " << root->kv.first << endl;
+            return false;
+        }
+        return _test(root->left) && _test(root->right);
+    }
     Node* _root = nullptr;
 };
-
-//检测代码，主要从两个方面检测，一个是高度差，一个是平衡因子是否有效
-template<class K, class V>
-int Height(AVLNode<K, V>* root)
-{
-    if(root == nullptr)
-    {
-        return 0;
-    }
-    int lh = Height(root->left);
-    int rh = Height(root->right);
-    return lh > rh ? lh + 1 : rh + 1;
-}
-
-template<class K, class V>
-bool Test(AVLNode<K, V>* root)
-{
-    if(root == nullptr)
-    {
-        return true;
-    }
-    int diff = - Height(root->left) + Height(root->right);
-    if(diff != 0 && diff != 1 && diff != -1)
-    {
-        cout << "error with the height of the tree" << endl;
-        return false;
-    }
-    if(diff != root->bf)
-    {
-        cout << "error with the bf:" << root->key << endl;
-        return false;
-    }
-    return Test(root->left) && Test(root->right);
-}
 
 int main()
 {
     AVLTree<int, int> at;
-    at.insert(1, 1);
-    at.insert(4, 1);
-    at.insert(2, 1);
-    at.insert(8, 1);
-    at.insert(6, 1);
-    at.insert(5, 1);
-    at.insert(7, 1);
+    at.insert(make_pair(1, 1));
+    at.insert(make_pair(8, 1));
+    at.insert(make_pair(4, 1));
+    at.insert(make_pair(7, 1));
+    at.insert(make_pair(6, 1));
+    at.insert(make_pair(11, 1));
+    at.insert(make_pair(3, 1));
+    at.insert(make_pair(9, 1));
+
     at.inOrder();
-    if(Test<int, int>(at.root()))
+
+    if(at.test())
     {
-        cout << "avl 数没有异常" << endl;
+        cout << "nothing wrong with the avl tree" << endl;
     }
     return 0;
 }
